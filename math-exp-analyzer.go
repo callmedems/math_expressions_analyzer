ed87388503a97787bc09fdc8d839e25fdd1c3331
callmedems <[demmi.zepeda@gmail.com]>
2025-05-28 19:03:16 -0600

    add main func

--- math-exp-analyzer.go
+++ math-exp-analyzer.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Gopher!")
+	// para leer los archivos
+	inputFile := flag.String("i", "", "Expressions.txt")
+	outputFile := flag.String("o", "", "Results.txt")
+	flag.Parse()
+
+	input, err := os.Open(*inputFile)
+	output, err = os.Create(*outputFile)
+
+	scanner := bufio.NewScanener(input)
+	writer := bufio.NewReadWriter(output)
+	expresionNum := 1
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		valid := validarExpresion(line)
+		status := "Valid"
+		if valid {
+			status = "Valid"
+		}
+
+		writer.WriteString(fmt.Sprintf("Expresion %d: %s - %s\n", expresionNum, line, status))
+
+		writer.Flush()
+	}
+}
+
+func validarExpresion(expr string) bool {
+	// usamos una pila para checar paréntesis y corchetes balaceads
+	stack := []rune{}
+	i := 0 //para iterar caracteres
+	length := len(expr)
 }
